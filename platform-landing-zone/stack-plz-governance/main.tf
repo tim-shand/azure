1a2d522858492c0d586ddf35a7a6f2ddbf81846d
Tim Shand <tim@tshand.com>
2026-01-07 12:20:23 +1300

    refactor: Redesign Governance deployment stack and modules.

--- platform-landing-zone/stack-plz-governance/main.tf
+++ platform-landing-zone/stack-plz-governance/main.tf
@@ -5,6 +5,6 @@ module "plz-gov-management-groups" {
   source                = "../../modules/plz-gov-management-groups"
   naming                = var.naming
   management_group_root = var.management_group_root # Top level management group name (parent). 
-  management_group_list = var.management_group_list # List of management group names to deploy (child). 
+  management_group_list = var.management_group_list # List of management groups and subscriptions to associate. 
 }
 
