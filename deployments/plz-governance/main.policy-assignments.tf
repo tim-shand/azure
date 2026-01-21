5be030a572cc5886d21d87fa32e7b645bad1e1d8
Tim Shand <tim@tshand.com>
2026-01-21 22:18:46 +1300

    chore: Minor change to order of code lines in governance stack. No impact.

--- deployments/plz-governance/main.policy-assignments.tf
+++ deployments/plz-governance/main.policy-assignments.tf
@@ -10,12 +10,12 @@ module "naming_policy_initiative_builtin" {
 
 # Policies: Builtin Initiatives - Assign built-in policy initiatives provided by list of display names to target management group.  
 module "policy_initiatives_builtin" {
-  count                      = var.policy_initiatives_builtin_enable ? 1 : 0 # Enable policy assignment (turns it on/off). 
   source                     = "../../modules/gov-policy-initiatives-builtin"
-  naming_prefix              = module.naming_policy_initiative_builtin # Pass in naming prefix for policy initiative display names. 
-  builtin_initiatives        = var.policy_initiatives_builtin          # List of builtin initiative display names. 
-  enforce                    = var.policy_initiatives_builtin_enforce  # Enforce policy controls (audit vs enforce).
-  target_management_group_id = module.management_groups.mg_root_id     # Target management group for assignment. 
+  count                      = var.policy_initiatives_builtin_enable ? 1 : 0 # Enable policy assignment (turns it on/off). 
+  naming_prefix              = module.naming_policy_initiative_builtin       # Pass in naming prefix for policy initiative display names. 
+  builtin_initiatives        = var.policy_initiatives_builtin                # List of builtin initiative display names. 
+  enforce                    = var.policy_initiatives_builtin_enforce        # Enforce policy controls (audit vs enforce).
+  target_management_group_id = module.management_groups.mg_root_id           # Target management group for assignment. 
 }
 
 # # Governance: Policies - Custom Definitions
