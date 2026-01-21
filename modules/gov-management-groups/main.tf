d8f1635b86ba4856ef515f5972ee28bee397f302
Tim Shand <tim@tshand.com>
2026-01-21 20:24:54 +1300

    refactor: Split local variables from main.tf into own file.

--- modules/gov-management-groups/main.tf
+++ modules/gov-management-groups/main.tf
@@ -1,49 +1,6 @@
-locals {
-  # Map matching subscriptions to management groups based on first three segments of the subscription ID. 
-  management_groups_subs_level1 = {
-    for mg, details in var.management_groups_level1 :
-    mg => {
-      display_name = details.display_name
-      subscriptions = [
-        for sub in var.subscriptions : sub.subscription_id
-        if contains(details.subscription_id_filter, join("-", slice(split("-", sub.subscription_id), 0, 3)))
-      ]
-    }
-  }
-  management_groups_subs_level2 = {
-    for mg, details in var.management_groups_level2 :
-    mg => {
-      display_name = details.display_name
-      parent_key   = details.parent_mg_name # Map static 'parent_mg_name' value to L1 MG key. 
-      subscriptions = [
-        for sub in var.subscriptions : sub.subscription_id
-        if contains(details.subscription_id_filter, join("-", slice(split("-", sub.subscription_id), 0, 3)))
-      ]
-    }
-  }
-  management_groups_subs_level3 = {
-    for mg, details in var.management_groups_level3 :
-    mg => {
-      display_name = details.display_name
-      parent_key   = details.parent_mg_name # Map static 'parent_mg_name' value to L2 MG key. 
-      subscriptions = [
-        for sub in var.subscriptions : sub.subscription_id
-        if contains(details.subscription_id_filter, join("-", slice(split("-", sub.subscription_id), 0, 3)))
-      ]
-    }
-  }
-  management_groups_subs_level4 = {
-    for mg, details in var.management_groups_level4 :
-    mg => {
-      display_name = details.display_name
-      parent_key   = details.parent_mg_name # Map static 'parent_mg_name' value to L3 MG key. 
-      subscriptions = [
-        for sub in var.subscriptions : sub.subscription_id
-        if contains(details.subscription_id_filter, join("-", slice(split("-", sub.subscription_id), 0, 3)))
-      ]
-    }
-  }
-}
+#=============================================================#
+# Platform LZ: Governance - Management Groups & Subscriptions
+#=============================================================#
 
 # Management Groups: Top-level management group for the organisation. 
 resource "azurerm_management_group" "root" {
@@ -83,6 +40,6 @@ resource "azurerm_management_group" "level4" {
   for_each                   = local.management_groups_subs_level4
   name                       = lower("${var.global.naming.org_code}-${each.key}-mg")     # Force lower-case for resource name. 
   display_name               = title(each.value.display_name)                            # Use map key for MG display name. 
-  parent_management_group_id = azurerm_management_group.level2[each.value.parent_key].id # Use string value to map to L3 MG key for nesting.  
+  parent_management_group_id = azurerm_management_group.level3[each.value.parent_key].id # Use string value to map to L3 MG key for nesting.  
   subscription_ids           = each.value.subscriptions                                  # Assign mapped subscriptions. 
 }
